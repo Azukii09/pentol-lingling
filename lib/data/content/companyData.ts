f72c6c141c74e25db0c1d7c14200a2fe3df34a12
Azukii09 <azuknisianta@gmail.com>
2025-08-30 17:53:53 +0700

    ## - Update company data: modify name, address, contact details, and social links.

--- lib/data/content/companyData.ts
+++ lib/data/content/companyData.ts
@@ -1,17 +1,16 @@
-export const companyName = "PT Ulaweng Mutiara Logistik"
-export const address = "Jl. Gorontalo V No.26B, RT.6/RW.1, Sungai Bambu, Kec. Tj. Priok, Jkt Utara, Daerah Khusus Ibukota Jakarta 14330"
-export const address2 = "Jl. Poros Pacellekang Kec.Pattallasang Kab.Gowa Blok R N0.5, Sulawesi Selatan"
-export const phone = "(+62) 877-7831-5326"
-export const email = "ulaweng.mutiara.log@gmail.com"
+export const companyName = "Pentol Ling Ling"
+export const address = "Kepohbaru, Bojonegoro, Jawa Timur"
+export const phone = "0812-3094-9938 / 0821-4341-0091"
+export const email = "-"
 export const social = {
-    whatsapp: "https://wa.me/6287778315326",
-    instagram: "https://instagram.com/startin",
-    linkedin: "https://linkedin.com/company/startin",
-    facebook: "https://facebook.com/startin",
-    twitter: "https://twitter.com/startin",
-    youtube: "https://youtube.com/startin",
-    github: "https://github.com/startin",
-    pinterest: "https://pinterest.com/startin",
-    snapchat: "https://snapchat.com/startin",
-    tiktok: "https://tiktok.com/startin",
+    whatsapp: "https://wa.me/6281230949938",
+    instagram: "https://www.instagram.com/pentollingling/",
+    linkedin: "-",
+    facebook: "https://www.facebook.com/pentol.ling.ling.2025",
+    twitter: "-",
+    youtube: "-",
+    github: "-",
+    pinterest: "-",
+    snapchat: "-",
+    tiktok: "https://www.tiktok.com/@pentol_lingling",
 }
