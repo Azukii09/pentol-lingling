139e01514b35462501dbabb25cf4704f017af76f
Azukii09 <azuknisianta@gmail.com>
2025-09-01 19:13:22 +0700

    ## - Add localization to About component and update `id` and `en` messages. Integrate `useTranslations` for dynamic text rendering.

--- component/ui/landing/about/MainAbout.tsx
+++ component/ui/landing/about/MainAbout.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 import useWindowWidth from "@/hook/useWindowWidth";
 import TitleArrow from "@/component/util/TitleArrow";
 import {amatic_sc, poppins} from "@/lib/font/font";
+import {useTranslations} from "next-intl";
 
 export default function MainAbout() {
     const width = useWindowWidth()
-
+    const tAbout = useTranslations('About')
     return (
         <section className={`${poppins.className} w-full global-padding mx-auto py-6 min-h-[calc(100vh-80px)] text-quaternary`}>
-            <TitleArrow text={"about us"} size={width}/>
+            <TitleArrow text={tAbout('about')} size={width}/>
             <h1 className={`${amatic_sc.className} text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center font-semibold`}>
-                Learn More <span className={"text-rose-600"}>About Us</span>
+                {tAbout('learnMore')} <span className={"text-rose-600"}>{tAbout('about')}</span>
             </h1>
             <div className={"flex flex-col gap-4 mt-10 p-4 lg:flex-row items-center justify-between"}>
                 <div className={"w-full lg:w-1/2"}>gambar tes</div>
