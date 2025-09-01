b42980ec00d6a213c5b72529d59b8e749149c853
Azukii09 <azuknisianta@gmail.com>
2025-09-01 17:36:28 +0700

    ## - Add sticky header functionality to Navigation and adjust layout styling.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,7 +3,7 @@ import {useTranslations} from 'next-intl';
 export default function HomePage() {
     const t = useTranslations('HomePage');
     return(
-        <div className={"w-full h-screen flex justify-center items-center global-padding"}>
+        <div className={"w-full h-screen flex justify-center global-padding"}>
             <h1>{t('title')}</h1>
         </div>
     )
--- component/layout/navigation/Navigation.tsx
+++ component/layout/navigation/Navigation.tsx
@@ -1,17 +1,39 @@
 'use client'
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import MobileToggleNavigation from "@/component/layout/navigation/mobile/MobileToggleNavigation";
 import Logo from "@/component/util/Logo";
 import LanguageChanger from "@/component/util/LanguageChanger";
 import DesktopNavigation from "@/component/layout/navigation/desktop/DesktopNavigation";
 
 export default function Navigation() {
+    const [heading, setHeading] = useState(false);
+
+    const scrollHeader = ()=>{
+        if(window.scrollY > 20){
+            setHeading(true);
+        }else {
+            setHeading(false);
+        }
+    }
+
+    useEffect(()=>{
+        window.addEventListener("scroll", scrollHeader);
+        return ()=>{
+            window.removeEventListener("scroll", scrollHeader);
+        }
+    },[])
+
+    console.log(heading)
+
     return (
         <>
             {/*large*/}
-            <div className="hidden md:flex">
-                <nav className={"global-padding w-full flex text-lg justify-between items-center bg-white text-primary py-4 shadow-sm shadow-slate-200"}>
+            <div className="hidden md:flex relative">
+                <nav className={`
+                            ${heading && "fixed z-50"}
+                            global-padding w-full flex text-lg justify-between items-center bg-white text-primary py-4 shadow-sm shadow-slate-200`}
+                >
                     <div className={"w-1/4"}>
                         <Logo/>
                     </div>
@@ -25,9 +47,9 @@ export default function Navigation() {
             </div>
 
             {/*mobile*/}
-            <div className="flex md:hidden">
+            <div className="flex md:hidden relative">
                 {/*navigation parent*/}
-                <nav className={"global-padding w-full flex justify-between items-center py-2 bg-white text-primary relative shadow-sm shadow-slate-200"}>
+                <nav className={`${heading ? "fixed z-50":"relative"} global-padding w-full flex justify-between items-center py-2 bg-white text-primary shadow-sm shadow-slate-200`}>
                     {/*logo*/}
                     <Logo/>
                     <MobileToggleNavigation/>
